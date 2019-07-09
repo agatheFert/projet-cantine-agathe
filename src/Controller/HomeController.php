f9ee57e6ca94b7c25af797513621efc8de51b674
agatheFert <agathefert60@gmail.com>
2019-07-09 17:35:18 +0200

    Installation Annotations + Apache-pack (.htaccess)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,10 +6,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
 
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     *
+     */
     public function home():Response
     {
       // return new Response('<h1>Réponse Symfony : page accueil</h1>');
